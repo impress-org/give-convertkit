624051a600b864a3f7f18fcc90cfd6b29befadb9
raftaar1191 <raftaar1191@gmail.com>
2018-09-03 18:40:49 +0530

    fix(admin-plugins): update plugin activation

--- includes/give-convertkit-activation.php
+++ includes/give-convertkit-activation.php
@@ -14,100 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Give ConvertKit Activation Banner
- *
- * Includes and initializes Give activation banner class.
- *
- * @since 1.0
- */
-function give_convertkit_activation_banner() {
-
-	// Check for if give plugin activate or not.
-	$is_give_active = defined( 'GIVE_PLUGIN_BASENAME' ) ? is_plugin_active( GIVE_PLUGIN_BASENAME ) : false;
-
-	//Check to see if Give is activated, if it isn't deactivate and show a banner
-	if ( current_user_can( 'activate_plugins' ) && ! $is_give_active ) {
-
-		add_action( 'admin_notices', 'give_convertkit_activation_notice' );
-
-		//Don't let this plugin activate
-		deactivate_plugins( GIVE_CONVERTKIT_BASENAME );
-
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
-
-		return false;
-
-	}
-
-	//Check minimum Give version
-	if ( defined( 'GIVE_VERSION' ) && version_compare( GIVE_VERSION, GIVE_CONVERTKIT_MIN_GIVE_VERSION, '<' ) ) {
-
-		add_action( 'admin_notices', 'give_convertkit_min_version_notice' );
-
-		//Don't let this plugin activate
-		deactivate_plugins( GIVE_CONVERTKIT_BASENAME );
-
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
-
-		return false;
-
-	}
-
-	// Check for activation banner inclusion.
-	if ( ! class_exists( 'Give_Addon_Activation_Banner' )
-	     && file_exists( GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php' )
-	) {
-
-		include GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php';
-	}
-
-	// Initialize activation welcome banner.
-	if ( class_exists( 'Give_Addon_Activation_Banner' ) ) {
-
-		//Only runs on admin
-		$args = array(
-			'file'              => __FILE__,
-			'name'              => esc_html__( 'ConvertKit', 'give-convertkit' ),
-			'version'           => GIVE_CONVERTKIT_VERSION,
-			'settings_url'      => admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=addons&section=convertkit-settings' ),
-			'documentation_url' => 'http://docs.givewp.com/addon-convertkit',
-			'support_url'       => 'https://givewp.com/support/',
-			'testing'           => false
-		);
-
-		new Give_Addon_Activation_Banner( $args );
-	}
-
-	return false;
-
-}
-
-add_action( 'admin_init', 'give_convertkit_activation_banner' );
-
-/**
- * Notice for No Core Activation
- *
- * @since 1.0
- */
-function give_convertkit_activation_notice() {
-	echo '<div class="error"><p>' . __( '<strong>Activation Error:</strong> You must have the <a href="https://givewp.com/" target="_blank">Give</a> plugin installed and activated for the ConvertKit add-on to activate.', 'give-convertkit' ) . '</p></div>';
-}
-
-/**
- * Notice for No Core Activation
- *
- * @since 1.0
- */
-function give_convertkit_min_version_notice() {
-	echo '<div class="error"><p>' . sprintf( __( '<strong>Activation Error:</strong> You must have <a href="%s" target="_blank">Give</a> version %s+ for the ConvertKit add-on to activate.', 'give-convertkit' ), 'https://givewp.com', GIVE_CONVERTKIT_MIN_GIVE_VERSION ) . '</p></div>';
-}
-
-
-/**
  * Plugins row action links.
  *
  * @since 1.0
@@ -136,7 +42,7 @@ add_filter( 'plugin_action_links_' . GIVE_CONVERTKIT_BASENAME, 'give_convertkit_
  *
  * @since 1.0
  *
- * @param array  $plugin_meta An array of the plugin's metadata.
+ * @param array $plugin_meta An array of the plugin's metadata.
  * @param string $plugin_file Path to the plugin file, relative to the plugins directory.
  *
  * @return array
