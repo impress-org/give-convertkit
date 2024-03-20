fa160c7d434e13e76b950f435b163643160ce895
Joshua Dinh <josh@joshuadinh.tech>
2024-03-20 14:31:50 -0700

    refactor: update placeholder and notice with accurate data

--- src/FormExtension/FormBuilder/Block/BlockPlaceholder/index.tsx
+++ src/FormExtension/FormBuilder/Block/BlockPlaceholder/index.tsx
@@ -1,33 +1,40 @@
-import {CheckboxControl} from '@wordpress/components';
-import {__} from '@wordpress/i18n';
-import './styles.scss';
-import {getWindowData} from '../window';
-import {createInterpolateElement} from '@wordpress/element';
+import { CheckboxControl } from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
+import "./styles.scss";
+import { getWindowData } from "../window";
+import { createInterpolateElement } from "@wordpress/element";
 
 /**
- * @since 2.0.0
+ * @unreleased
  */
-export default function BlockPlaceholder({checked, label}) {
-	const {requiresSetup, settingsUrl} = getWindowData();
+export default function BlockPlaceholder({ defaultChecked, label }) {
+  const { requiresSetup, settingsUrl } = getWindowData();
 
-	return (
-		<div
-			className={`givewp-mailchimp-block-placeholder
-			${requiresSetup && 'givewp-mailchimp-block-placeholder--invalid'}`}
-		>
-			{requiresSetup ? (
-				createInterpolateElement(
-					__(
-						'This block requires additional setup. Go to your <a>Settings</a> to connect your Mailchimp account.',
-						'give'
-					),
-					{
-						a: <a href={settingsUrl} target="_blank" rel="noopener noreferrer" />,
-					}
-				)
-			) : (
-				<CheckboxControl checked={checked} label={label} onChange={null} disabled={true} />
-			)}
-		</div>
-	);
+  return (
+    <div
+      className={`givewp-convertkit-block-placeholder
+			${requiresSetup && "givewp-convertkit-block-placeholder--invalid"}`}
+    >
+      {requiresSetup ? (
+        createInterpolateElement(
+          __(
+            "This block requires additional setup. Go to your <a>Settings</a> to connect your ConvertKit account.",
+            "give"
+          ),
+          {
+            a: (
+              <a href={settingsUrl} target="_blank" rel="noopener noreferrer" />
+            ),
+          }
+        )
+      ) : (
+        <CheckboxControl
+          checked={defaultChecked}
+          label={label}
+          onChange={null}
+          disabled={true}
+        />
+      )}
+    </div>
+  );
 }
