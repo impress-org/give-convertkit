3983d25842658ede9d846907d60ef624d6155ab2
Joshua Dinh <josh@joshuadinh.tech>
2024-03-20 14:34:42 -0700

    refactor: replace window declaration, add placeholder & notice

--- src/FormExtension/FormBuilder/Block/edit.tsx
+++ src/FormExtension/FormBuilder/Block/edit.tsx
@@ -1,65 +1,100 @@
-import {__} from '@wordpress/i18n';
-import {BlockEditProps} from '@wordpress/blocks';
-import {InspectorControls} from '@wordpress/block-editor';
-import {PanelBody, CheckboxControl, SelectControl, TextControl, ToggleControl} from '@wordpress/components';
+import { __ } from "@wordpress/i18n";
+import { BlockEditProps } from "@wordpress/blocks";
+import { InspectorControls } from "@wordpress/block-editor";
+import {
+  PanelBody,
+  SelectControl,
+  TextControl,
+  ToggleControl,
+} from "@wordpress/components";
+import BlockPlaceholder from "./BlockPlaceholder";
+import { getWindowData } from "./window";
+import BlockNotice from "./BlockNotice";
 
-declare const window: {
-        GiveConvertKit: {
-            forms?: Array<{ id: string; name: string }>,
-            tags?: Array<{ id: string; name: string }>,
-        }
-} & Window
-
-const formsListOptions = window.GiveConvertKit.forms.map(function(form) {
-    return { value: form.id, label: form.name };
+const { forms, tags } = getWindowData();
+const formsListOptions = forms.map(function ({ id, name }) {
+  return { value: id, label: name };
 });
-const tagsListOptions = window.GiveConvertKit.tags.map(function(tag) {
-    return { value: tag.id, label: tag.name };
+const tagsListOptions = tags.map(function ({ id, name }) {
+  return { value: id, label: name };
 });
 
 /**
  * @unreleased
  */
-export default function Edit({attributes, setAttributes}: BlockEditProps<any>) {
-    const {defaultChecked, label, selectedForm, tagSubscribers} = attributes;
-    return (
-        <>
-            <div className={'givewp-convertkit-block-placeholder'}>
-                <CheckboxControl checked={defaultChecked} label={label} onChange={null} disabled={true} />
+export default function Edit({
+  attributes,
+  setAttributes,
+}: BlockEditProps<any>) {
+  const { defaultChecked, label, selectedForm, tagSubscribers } = attributes;
+  const { settingsUrl, requiresSetup } = getWindowData();
+  return (
+    <>
+      <BlockPlaceholder {...{ defaultChecked, label }} />
+      <InspectorControls>
+        <PanelBody
+          title={__("Field Settings", "give-convertkit")}
+          initialOpen={true}
+        >
+          {requiresSetup ? (
+            <BlockNotice
+              title={__("ConvertKit requires setup", "give")}
+              description={__(
+                "This block requires your settings to be configured in order to use.",
+                "give"
+              )}
+              anchorText={__("Connect your ConvertKit account", "give")}
+              href={settingsUrl}
+            />
+          ) : (
+            <div className={"givewp-convertkit-controls"}>
+              <TextControl
+                label={__("Custom Label", "give-convertkit")}
+                value={label}
+                help={__(
+                  "Customize the label for the ConvertKit opt-in checkbox",
+                  "give-convertkit"
+                )}
+                onChange={(value) => setAttributes({ label: value })}
+              />
+
+              <ToggleControl
+                label={__("Opt-in Default", "give-convertkit")}
+                checked={defaultChecked}
+                onChange={() =>
+                  setAttributes({ defaultChecked: !defaultChecked })
+                }
+                help={__(
+                  "Customize the newsletter opt-in option for this form.",
+                  "give-convertkit"
+                )}
+              />
+
+              <SelectControl
+                label={__("ConvertKit Form", "give-convertkit")}
+                value={selectedForm ?? ""}
+                onChange={(value) => setAttributes({ selectedForm: value })}
+                options={[
+                  {
+                    value: "",
+                    label: __("Select a Form", "give-convertkit"),
+                    disabled: true,
+                  },
+                  ...formsListOptions,
+                ]}
+              />
+
+              <SelectControl
+                multiple={true}
+                label={__("Tag Subscribers", "give-convertkit")}
+                value={tagSubscribers}
+                onChange={(value) => setAttributes({ tagSubscribers: value })}
+                options={tagsListOptions}
+              />
             </div>
-            <InspectorControls>
-                <PanelBody title={__('Field Settings', 'give-convertkit')} initialOpen={true}>
-                    <div className={'givewp-convertkit-controls'}>
-                        <TextControl
-                            label={__('Custom Label', 'give-convertkit')}
-                            value={label}
-                            help={__('Customize the label for the ConvertKit opt-in checkbox', 'give-convertkit')}
-                            onChange={(value) => setAttributes({label: value})}
-                        />
-
-                        <ToggleControl
-                            label={__('Opt-in Default', 'give-convertkit')}
-                            checked={defaultChecked}
-                            onChange={() => setAttributes({defaultChecked: !defaultChecked})}
-                            help={__(
-                                'Customize the newsletter opt-in option for this form.',
-                                'give-convertkit'
-                            )}
-                        />
-
-                        <SelectControl label={__('ConvertKit Form', 'give-convertkit')} value={selectedForm ?? ''} onChange={(value) => setAttributes({selectedForm: value})}
-                                       options={[
-                                           {value: '', label: __('Select a Form', 'give-convertkit'), disabled: true},
-                                           ...formsListOptions,
-                                       ]}
-                        />
-
-                        <SelectControl multiple={true} label={__('Tag Subscribers', 'give-convertkit')} value={tagSubscribers} onChange={(value) => setAttributes({tagSubscribers: value})}
-                                       options={tagsListOptions}
-                        />
-                    </div>
-                </PanelBody>
-            </InspectorControls>
-        </>
-    );
+          )}
+        </PanelBody>
+      </InspectorControls>
+    </>
+  );
 }
