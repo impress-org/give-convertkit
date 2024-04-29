095a4882df0f6aa971f815408dc51cb0473ba434
Joshua Dinh <josh@joshuadinh.tech>
2024-04-29 16:13:18 -0700

    refactor: update selectedForm to selectedForms array

--- src/FormExtension/FormBuilder/Block/metadata.ts
+++ src/FormExtension/FormBuilder/Block/metadata.ts
@@ -1,37 +1,38 @@
-import type {BlockConfiguration} from '@wordpress/blocks';
-import {__} from '@wordpress/i18n';
+import type { BlockConfiguration } from "@wordpress/blocks";
+import { __ } from "@wordpress/i18n";
 
 /**
  * @unreleased
  */
 const metadata: BlockConfiguration = {
-    name: 'givewp-convertkit/convertkit',
-    title: __('ConvertKit', 'give-convertkit'),
-    description: __(
-        'Easily integrate ConvertKit opt-ins within your Give donation forms.',
-        'give-convertkit'
-    ),
-    category: 'addons',
-    supports: {
-        multiple: false,
+  name: "givewp-convertkit/convertkit",
+  title: __("ConvertKit", "give-convertkit"),
+  description: __(
+    "Easily integrate ConvertKit opt-ins within your Give donation forms.",
+    "give-convertkit"
+  ),
+  category: "addons",
+  supports: {
+    multiple: false,
+  },
+  attributes: {
+    label: {
+      type: "string",
+      default: __("Subscribe to our newsletter?", "give-convertkit"),
     },
-    attributes: {
-        label: {
-            type: 'string',
-            default: __('Subscribe to newsletter?', 'give'),
-        },
-        defaultChecked: {
-            type: 'boolean',
-            default: true,
-        },
-        selectedForm: {
-            type: 'string',
-        },
-        tagSubscribers: {
-            type: 'array',
-            default: [],
-        },
+    defaultChecked: {
+      type: "boolean",
+      default: true,
     },
+    selectedForms: {
+      type: "array",
+      default: [],
+    },
+    tagSubscribers: {
+      type: "array",
+      default: [],
+    },
+  },
 };
 
 export default metadata;
